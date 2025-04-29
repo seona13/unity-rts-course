04817fb4831a36ebdf645177ab1e4f36492c8da1
Seona <seona13@gmail.com>
2025-04-29 14:44:31 +1000

    1.7 - Camera Zoom
    We’ll implement a WarCraft III-style Zoom with Cinemachine and the new input system!

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,10 +6,76 @@ using UnityEngine.InputSystem;
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] Transform cameraTarget;
+    [SerializeField] CinemachineCamera cinemachineCamera;
     [SerializeField] float keyboardPanSpeed = 5;
+    [SerializeField] float zoomSpeed = 1;
+    [SerializeField] float minZoomDistance = 7.5f;
+
+    CinemachineFollow cinemachineFollow;
+    float zoomStartTime;
+    Vector3 startingFollowOffset;
+
+
+    void Awake()
+    {
+        if (cinemachineCamera.TryGetComponent(out cinemachineFollow) == false)
+        {
+            Debug.LogError("Cinemachine Camera did not have CinemachineFollow. Zoom functionality will not work!");
+        }
+
+        startingFollowOffset = cinemachineFollow.FollowOffset;
+    }
 
 
     void Update()
+    {
+        HandlePanning();
+        HandleZooming();
+    }
+
+
+    void HandleZooming()
+    {
+        if (ShouldSetZoomStartTime())
+        {
+            zoomStartTime = Time.time;
+        }
+
+        Vector3 targetFollowOffset;
+        float zoomTime = Mathf.Clamp01((Time.time - zoomStartTime) * zoomSpeed);
+
+        if (Keyboard.current.endKey.isPressed)
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                minZoomDistance,
+                cinemachineFollow.FollowOffset.z
+                );
+        }
+        else
+        {
+            targetFollowOffset = new Vector3(
+                cinemachineFollow.FollowOffset.x,
+                startingFollowOffset.y,
+                cinemachineFollow.FollowOffset.z
+                );
+        }
+
+        cinemachineFollow.FollowOffset = Vector3.Slerp(
+            cinemachineFollow.FollowOffset,
+            targetFollowOffset,
+            zoomTime
+            );
+    }
+
+
+    bool ShouldSetZoomStartTime()
+    {
+        return Keyboard.current.endKey.wasPressedThisFrame || Keyboard.current.endKey.wasReleasedThisFrame;
+    }
+
+
+    void HandlePanning()
     {
         Vector2 moveAmount = Vector2.zero;
 
